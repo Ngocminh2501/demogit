c9ee7771d43753cbdb6ce68686b6388bafcf48ba
Ngocminh2501 <duongthongocminh@gmail.com>
2024-08-22 23:08:55 +0700

    add init project

--- home.php
+++ home.php
@@ -4,14 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        img{
+            width: 500px;
+            height: 500px;
+            object-fit: cover;
+        }
+    </style>
 </head>
 <body>
-    <?php
-    echo "Hello this is PHP script";
-    echo '<h1 style="color: blue;text-align: center;">Xin chào TMU</h1>';
-    ?>
 
+<?php
+echo "Hello, this is PHP script";
+echo '<h1 style="color: blue; text-align: center;">Xin chào TMU</h1>';
+echo'<img src="avatar-anh-meo-cute-3.jpg" alt="">'
 
-    
+?>
+ 
+ <p>Xin chào</p>
+
 </body>
 </html>
